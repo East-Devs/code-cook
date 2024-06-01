354559fa28b9539985259e187c4bc43021c107f7
syedHasnain0099 <syedhasnain0099@gmail.com>
2024-06-02 04:49:26 +0500

    connected prisma,prsima,postgres,frontend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
+dotenv.config();
 const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users); // Ensure you're sending JSON response
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Data", data);
+        setMessage(JSON.stringify(data));
+      })
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
+  return <div>{message}</div>;
 };
 
 export default Home;
