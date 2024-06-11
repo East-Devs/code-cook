5b43e00a350bb41edca3863d8fceaf5f77351eb2
syedHasnain0099 <syedhasnain0099@gmail.com>
2024-06-11 15:00:16 +0500

    removed commented lines

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-
-// async function main() {
-//   //   const user = await prisma.user.create({
-//   //     data: { name: "Kyle", email: "Kyle@gmail.com", password: "123456789" },
-//   //   });
-//   //   console.log(user);
-// }
-
-// main()
-//   .catch((e) => console.log(e.message))
-//   .finally(async () => {
-//     await prisma.$disconnect;
-//   });
-
 export default prisma;
